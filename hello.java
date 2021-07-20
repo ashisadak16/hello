7b6227e3b7e6363a1ebe973f68002a62d96bff94
ashis <root@ip-172-31-0-166.us-east-2.compute.internal>
2021-07-20 16:56:13 +0000

    2nd commit

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-     System.out.println("Hello Java my account");
+     System.out.println("Hello Java my accounturei,sure you can bliv,01");
     }
 }
